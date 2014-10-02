211118a7c8b120835034267b98b20501bb5d58d4
Brad Bell <brad@pixelandtonic.com>
2014-10-02 13:59:12 -0700

    2.2.2592

--- app/vendor/twig/twig/lib/Twig/Template.php
+++ app/vendor/twig/twig/lib/Twig/Template.php
@@ -455,6 +455,17 @@ abstract class Twig_Template implements Twig_TemplateInterface
             $this->env->getExtension('sandbox')->checkMethodAllowed($object, $method);
         }
 
+        /* BEGIN HACK */
+
+        // Convert any Twig_Markup arguments back to strings
+        foreach ($arguments as $key => $value) {
+            if ($value instanceof Twig_Markup) {
+                $arguments[$key] = (string) $value;
+            }
+        }
+
+        /* END HACK */
+
         // Some objects throw exceptions when they have __call, and the method we try
         // to call is not supported. If ignoreStrictCheck is true, we should return null.
         try {
