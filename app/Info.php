ba6beca1ceee5cdc293d28732e5d647e8f505565
Brad Bell <brad@pixelandtonic.com>
2015-07-21 11:36:21 -0700

    2.4.2677

--- app/Info.php
+++ app/Info.php
@@ -2,9 +2,9 @@
 namespace Craft;
 
 define('CRAFT_VERSION', '2.4');
-define('CRAFT_BUILD', '2675');
+define('CRAFT_BUILD', '2677');
 define('CRAFT_SCHEMA_VERSION', '2.4.0');
-define('CRAFT_RELEASE_DATE', '1437082567');
+define('CRAFT_RELEASE_DATE', '1437502792');
 define('CRAFT_MIN_BUILD_REQUIRED', '2570');
 define('CRAFT_MIN_BUILD_URL', 'http://download.buildwithcraft.com/craft/2.1/2.1.2570/Craft-2.1.2570.zip');
 define('CRAFT_TRACK', 'stable');
